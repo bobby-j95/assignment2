9586650c10e3762ce81de4d283ea6312958b34ca
Robert Johns <robertjohns08@gmail.com>
2020-10-08 12:31:26 -0400

    Added classes to some files

--- src/main/java/com/meritamerica/assignment2/CDAccount.java
+++ src/main/java/com/meritamerica/assignment2/CDAccount.java
@@ -4,4 +4,26 @@ import java.util.Date;
 
 public class CDAccount {
 
+	CDAccount(CDOffering offering, double balance){
+		
+	}
+	double getBalance() {
+		
+	}
+	double getInterestRate() {
+		
+	}
+	int getTerm() {
+		
+	}
+	java.util.Date getStartDate(){
+		
+	}
+	long getAccountNumber() {
+		
+	}
+	double futureValue() {
+		
+	}
+
 }
