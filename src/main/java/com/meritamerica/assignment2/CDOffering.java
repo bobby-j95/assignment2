f1fbf09b68bea1b5374ae8fde74624bc8c914ab0
behulum <behulummd2018@gmail.com>
2020-10-08 16:56:49 -0400

    change2

--- src/main/java/com/meritamerica/assignment2/CDOffering.java
+++ src/main/java/com/meritamerica/assignment2/CDOffering.java
@@ -2,15 +2,33 @@ package com.meritamerica.assignment2;
 
 //created by behulum w 
 public class CDOffering {
+	
+private double interestRate;
+private int term;
 
-	CDOffering(int term, double interestRate){
+public CDOffering(int term, double interestRate) {
+	
+
+		this.interestRate = interestRate;
 		
-	}
-	int getTerm() {
+		this.term = term;	
 		
-	}
-	double getInterestRate() {
-		
-	}
+		switch(term) {
+		case 1:
+			interestRate = 0.018;
+			break;
+		case 2:
+			interestRate = 0.019;
+			break;
+		case 3:
+			interestRate = 0.020;
+			break;
+		case 5:
+			interestRate = 0.025;
+			break;
+		case 10:
+			interestRate = 0.022;
+			break;
+		}
 
 }
